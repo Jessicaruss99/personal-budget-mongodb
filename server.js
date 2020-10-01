fe36912f57954e1199032197929bcd04493a9da5
jessicaruss99 <jessielove321@hotmail.com>
2020-10-01 13:22:30 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,26 +1,11 @@
 const express = require('express');
+const budget = require('./budget.json');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-
-myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-]
-};
+// var myBudget = JSON.parse(budget);
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
